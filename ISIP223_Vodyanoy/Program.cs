0c9c19814ea6d3f5abaf30252f0aec0f8cf070ef
Андрей <@DESKTOP-RBJSBUR>
2025-10-08 11:17:47 +0300

    Введена система здоровья у сущности

--- ISIP223_Vodyanoy/Program.cs
+++ ISIP223_Vodyanoy/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class Entity 
+public  abstract class Entity 
 
 
 {
@@ -15,5 +15,18 @@ public class Entity
 
     public int Defence { get; set; }
 
+    public Entity(int maxhp, int hp, int attack, int defence)
+    {
+        MaxHP = maxhp; 
+        HP = hp;
+        Attack = attack;
+        Defence = defence;
+    }
+    public void Heal()
+    {
+        HP = Math.Min(MaxHP, HP + amount);
+    }
+
+    public bool IsAlive => HP > 0;
 
 }
