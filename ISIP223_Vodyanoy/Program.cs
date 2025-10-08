5c99945e3c560f8b570609c946513389bc4101de
Андрей <@DESKTOP-RBJSBUR>
2025-10-08 11:11:35 +0300

    ввод систмемы здоровья у игрока

--- ISIP223_Vodyanoy/Program.cs
+++ ISIP223_Vodyanoy/Program.cs
@@ -2,7 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class player
+public class Player
 {
+    public string Name { get; set; }
+    public double HP { get; set; }
 
+    public int MaxHP { get; set; }
+
+    public string Equip {  get; set; }
+
+    public string Weapon {  get; set; }
+
+    public Player(string name, double hp, string equip, string weapon)
+    {
+        Name = name;
+        HP = hp;
+        Equip = equip;
+        Weapon = weapon;
+    }
+
+    public void Heal()
+    {
+        HP = Math.Min(MaxHP, HP + amount);
+    }
+
+    public bool IsAlive => HP > 0;
 }
